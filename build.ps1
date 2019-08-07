3d0df292764c3e8464aff99a7cca6f811be4c428
Trevor Taubitz <terrabitz@protonmail.com>
2019-08-07 12:45:57 -0500

    ci: Set python binary based on OS type

--- build.ps1
+++ build.ps1
@@ -2,5 +2,13 @@ $ErrorActionPreference = 'Stop'
 
 Write-Host "Starting Build"
 
-& "$env:PYTHON\Scripts\pip" install --user pyinstaller
+if ($IsWindows) {
+    $env:Path += ";$env:PYTHON;$env:Python\Scripts"
+    refreshenv
+} elseif ($IsLinux) {
+    Set-Alias python "/usr/bin/python$env:PYTHON_VERSION"
+}
+
+python -m pip install --upgrade pip
+python -m pip install --user pyinstaller
 pyinstaller -F ./main.py
