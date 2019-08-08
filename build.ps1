83e6636571cc163ea7e2c5e9fe10e74de16b503a
Trevor Taubitz <terrabitz@protonmail.com>
2019-08-08 13:12:17 -0500

    ci: Append OS to binary

--- build.ps1
+++ build.ps1
@@ -3,8 +3,8 @@ Write-Host "Starting Build"
 pyinstaller -F ./main.py
 Write-Host $env:APPVEYOR_REPO_TAG.gettype().name
 if ($env:APPVEYOR_REPO_TAG -eq "true") {
-    $newName = "pyinstaller-${env:APPVEYOR_REPO_TAG_NAME}.exe"
+    $newName = "pyinstaller-${env:APPVEYOR_REPO_TAG_NAME}_win.exe"
 } else {
-    $newName = "pyinstaller-build${env:APPVEYOR_BUILD_VERSION}.exe"
+    $newName = "pyinstaller-build${env:APPVEYOR_BUILD_VERSION}_win.exe"
 }
 Move-Item ./dist/main.exe ./dist/$newName
