300f51461a43e18695bce23957b17ee9de9918fb
Trevor Taubitz <terrabitz@protonmail.com>
2019-08-08 12:32:22 -0500

    ci: Use alternate python install script

--- build.ps1
+++ build.ps1
@@ -2,16 +2,4 @@ $ErrorActionPreference = 'Stop'
 
 Write-Host "Starting Build"
 
-if ($IsWindows) {
-    Set-Alias python3 "$env:PYTHON\python.exe"
-    Set-Alias pip3 "$env:PYTHON\Scripts\pip.exe"
-    $env:PATH += ";C:\Users\appveyor\AppData\Roaming\Python\Python$env:PYTHON_VERSION\Scripts"
-}
-
-pip3 install --upgrade pip
-try {
-    pip3 install --user pyinstaller
-} catch {
-    # Do nothing
-}
 pyinstaller -F ./main.py
