cb371037339a1155a0cdaa0d847032d4ee202ccd
Trevor Taubitz <terrabitz@protonmail.com>
2019-08-08 12:42:14 -0500

    ci: Rename created executable

--- build.ps1
+++ build.ps1
@@ -1,3 +1,9 @@
 Write-Host "Starting Build"
 
 pyinstaller -F ./main.py
+if ($env:APPVEYOR_REPO_TAG) {
+    $newName = "pyinstaller-${env:APPVEYOR_REPO_TAG}.exe"
+} else {
+    $newName = "pyinstaller-build${env:APPVEYOR_BUILD_VERSION}.exe"
+}
+Move-Item ./dist/main.ext ./dist/$newName
